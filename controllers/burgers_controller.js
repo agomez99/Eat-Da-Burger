5db3c2d61a8ca48ba5e97328d54b6716da975e59
agomez99 <austine.gomez@yahoo.com>
2020-04-22 18:04:54 -0500

    remove comments

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,6 @@ router.get("/", function (req, res) {
     var hbsObject = {
       burgers: data,
     };
-    //console.log("router /", hbsObject);
     res.render("index", hbsObject);
   });
 });
@@ -32,7 +31,6 @@ router.put("/api/burgers/:id", function (req, res) {
     condition,
     function (result) {
       if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       }
       res.status(200).end();
@@ -42,7 +40,6 @@ router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
     burger.deleteOne(condition, function (result) {
       if (result.changedRowsRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
         return console.log("this fails");
       } else {
         res.status(200).end();
